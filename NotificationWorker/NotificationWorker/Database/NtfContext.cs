4d55fec9591b7e43c36ce71fdebcf66826501e40
Stepan Ryzhkov <clouztoyou@mail.ru>
2026-06-03 18:52:46 +0300

    Db addition

---
+++ NotificationWorker/NotificationWorker/Database/NtfContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotificationWorker.Models;
+
+namespace NotificationWorker.Database
+{
+    public class NtfContext : DbContext
+    {
+        public DbSet<NotificationInfo> Notifications { get; set; } = null!;
+        public NtfContext(DbContextOptions<NtfContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<NotificationInfo>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Id).HasColumnType("UUID");
+                entity.Property(e => e.Message).IsRequired(); 
+                entity.Property(e => e.Status)
+                      .HasConversion<string>()
+                      .HasMaxLength(20)
+                      .IsRequired();
+                entity.Property(e => e.SentAt).HasDefaultValueSql("CURRENT_TIMESTAMP");
+            });
+        }
+    }
+}
---
+++ NotificationWorker/NotificationWorker/Models/NotificationInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace NotificationWorker.Models
+{
+    public enum MessageStatus
+    {
+        Sent,
+        Failed,
+        Pending
+    }
+
+    public class NotificationInfo
+    {
+        public Guid Id { get; set; }
+        public int UserId { get; set; }
+        public string? Message { get; set; }
+        public MessageStatus Status { get; set; }
+        public DateTime SentAt { get; set; }
+    }
+}

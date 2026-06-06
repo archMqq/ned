b3e98bb3288e53427fe3e90d569f17830e9cc06c
Stepan Ryzhkov <clouztoyou@mail.ru>
2026-06-06 05:09:26 +0300

    Database repository add

---
+++ NotificationWorker/NotificationWorker/Repositories/IRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace NotificationWorker.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<int> SaveAsync(T entity);
+        Task<T> GetByIdASync(int id);
+    }
+}
---
+++ NotificationWorker/NotificationWorker/Repositories/NotificationRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotificationWorker.Database;
+using NotificationWorker.Models;
+using System.Data;
+
+namespace NotificationWorker.Repositories
+{
+    public class NotificationRepository : IRepository<NotificationInfo>
+    {
+        readonly NtfContext _context;
+        readonly ILogger<NotificationRepository> _logger;
+        public NotificationRepository(NtfContext context, ILogger<NotificationRepository> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<int> SaveAsync(NotificationInfo entity)
+        {
+            await _context.Notifications.AddAsync(entity);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException concEx)
+            {
+                _logger.LogError(concEx, "A concurrency error occurred while saving the notification.");
+            }
+            catch (DbUpdateException updEx)
+            {
+                _logger.LogError(updEx, "An error occurred while saving the notification.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unexpected error occurred while saving the notification.");
+            }
+
+            return entity.Id;
+        }
+
+        public async Task<NotificationInfo> GetByIdASync(int id)
+        {
+            var entity = await _context.Notifications.FirstOrDefaultAsync(x => x.Id == id);
+            if (entity == null)
+            {
+                _logger.LogInformation("An attempt to obtain information on unknown notification.");
+                throw new Exceptions.UnknownId("Attempt to obtain unknown notification");
+            }
+
+            return entity;
+        }
+    }
+}
